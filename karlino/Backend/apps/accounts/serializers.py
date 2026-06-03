503f168053c05c0808ed3335715e2fce7d45005f
Alikhoshakhlagh <alikhoshakhlagh90@gmail.com>
2026-06-03 14:43:12 +0330

    fix resetpassword jwt bug

--- karlino/Backend/apps/accounts/serializers.py
+++ karlino/Backend/apps/accounts/serializers.py
@@ -1,29 +1,45 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 
-
-class ResetPasswordSerializer(serializers.Serializer):
+class ResetPasswordSerializer(
+    serializers.Serializer
+):
 
     email = serializers.EmailField()
 
     new_password = serializers.CharField(
-        min_length=8,
+        write_only=True,
     )
 
     confirm_password = serializers.CharField(
-        min_length=8,
+        write_only=True,
     )
 
-    def validate(self, attrs):
+    def validate_new_password(
+        self,
+        value,
+    ):
+
+        validate_password(value)
+
+        return value
+
+    def validate(
+        self,
+        attrs,
+    ):
 
         if (
             attrs['new_password']
             != attrs['confirm_password']
         ):
 
-            raise serializers.ValidationError({
-                'confirm_password': (
-                    'Passwords do not match.'
-                )
-            })
+            raise serializers.ValidationError(
+                {
+                    'confirm_password': (
+                        'Passwords do not match.'
+                    )
+                }
+            )
 
         return attrs
